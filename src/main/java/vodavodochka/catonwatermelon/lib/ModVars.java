17176bbb7e46692d6249c17b101e3341350aedef
Deeplerg <47917846+deeplerg@users.noreply.github.com>
2026-07-18 01:53:42 +0500

    Bump version to 0.0.6

--- src/main/java/vodavodochka/catonwatermelon/lib/ModVars.java
+++ src/main/java/vodavodochka/catonwatermelon/lib/ModVars.java
@@ -5,5 +5,5 @@ public class ModVars {
 
     public static final String Mod_Name = "Cat On Watermelon";
     public static final String Mod_ID = "catonwatermelon";
-    public static final String Mod_Version = "0.0.5";
+    public static final String Mod_Version = "0.0.6";
 }
